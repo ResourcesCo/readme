d3b73ef5f562dc668f12422f9692b41849224181
Benjamin Atkin <ben@resources.co>
2021-01-14 10:29:41 -0500

    feat: add home page 🏡

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>README</h1>
+      <p>Your READMEs</p>
+      <p>Explore</p>
+    </>
+  )
+}
+
+export default HomePage
