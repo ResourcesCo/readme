719bab4b0d994365dcff35797a5333cf33b6a3a5
Benjamin Atkin <ben@resources.co>
2021-01-15 18:02:24 -0500

    feat: render markdown 📄

--- web/src/components/ReadmeCell/ReadmeCell.js
+++ web/src/components/ReadmeCell/ReadmeCell.js
@@ -1,3 +1,8 @@
+import unified from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeReact from 'rehype-react'
+
 export const QUERY = gql`
   query ReadmesQuery {
     redwood {
@@ -12,6 +17,20 @@ export const Empty = () => <div>Empty</div>
 
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
-export const Success = ({ redwood }) => {
-  return <pre>{JSON.stringify(redwood, null, 2)}</pre>
+const processor = unified()
+  .use(remarkParse)
+  .use(remarkRehype)
+  .use(rehypeReact, { createElement: React.createElement })
+
+export const Success = ({ redwood: { currentUser } }) => {
+  const text = `# Hello, world!
+
+  This is rendered using Remark and React! 🎉`
+
+  return (
+    <div>
+      <div>{processor.processSync(text).result}</div>
+      <pre>{JSON.stringify(currentUser, null, 2)}</pre>
+    </div>
+  )
 }
