902bf187323016e80f50668599b62461e9a6f08d
Benjamin Atkin <ben@resources.co>
2021-01-21 23:36:21 -0500

    style: add save button to README page ☑️

--- web/src/pages/ReadmePage/ReadmePage.js
+++ web/src/pages/ReadmePage/ReadmePage.js
@@ -1,11 +1,24 @@
 /** @jsx jsx */
-import { jsx, Textarea } from 'theme-ui'
+import { useState } from 'react'
+import { jsx, Textarea, Button, Flex, Spinner } from 'theme-ui'
 import AppLayout from '../../layouts/AppLayout/AppLayout'
 
 const ReadmePage = () => {
+  const [loading, setLoading] = useState(false)
+  const save = () => {
+    setLoading(true)
+  }
   return (
     <AppLayout title="New Readme">
       <Textarea sx={{ height: '80vh' }} />
+      <Flex sx={{ my: 3 }}>
+        <Button mr={2} onClick={save}>
+          Save
+          {loading && (
+            <Spinner sx={{ color: 'white', size: 18, ml: 2, mb: '-3px' }} />
+          )}
+        </Button>
+      </Flex>
     </AppLayout>
   )
 }
